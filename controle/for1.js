f1fafc14adb2cb7b7d82800ab17a696d4f735eb4
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-06-15 16:30:02 -0300

    break/continue, for e funcao
    O uso de break e continue
    o laco for e for in
    propriedades da funcao em javaScript

--- controle/for1.js
+++ controle/for1.js
@@ -3,6 +3,11 @@ while(contador <= 10){
     console.log(`contador = ${contador}.`)
     contador ++
 }
-for(let i; i <= 10; i++){
-    console.log(`i =${i}`)
+for(let i = 0; i <= 10; i++){
+    console.log(`i = ${i}`)
+}
+
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+for(i = 0; i < notas.length; i++){
+    console.log(`nota = ${notas[i]}`)
 }
