75d6c451d9463fc67ea35436a02b73922595aaa5
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-06-17 17:55:07 -0300

    funções anônimas
    mais alguns testes

--- funcao/funcoesanonimas.js
+++ funcao/funcoesanonimas.js
@@ -9,4 +9,14 @@ imprimirResultado(3,4)
 imprimirResultado(3, 4, soma)
 imprimirResultado(3, 4, function(x,y){return x- y})
 imprimirResultado(3 ,4, (x,y)=> x * y)
-imprimirResultado(100,2, function(x,y){return x/y})
+imprimirResultado(10,2, function(x,y){return x * y})
+
+const pessoa ={
+    nome: 'josuel',
+    falar: function(){
+        console.log(`Isso é o que o ${this.nome} diria...`)
+        console.log('não sei o que dizer...')
+    }
+}
+
+pessoa.falar()
