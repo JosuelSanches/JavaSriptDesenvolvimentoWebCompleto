57795118f1d264fe3b06fd6dc7b268cfd52951dc
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-06-17 17:58:37 -0300

    Testando funções anônimas
    corrigindo erros em função anônima

--- funcao/funcoesanonimas.js
+++ funcao/funcoesanonimas.js
@@ -3,13 +3,13 @@ const soma = function(x,y){
 }
 
 const imprimirResultado = function(a, b, operacao = soma){
-    console.log(operacao(3,4))
+    console.log(operacao(a,b))
 }
 imprimirResultado(3,4)
 imprimirResultado(3, 4, soma)
 imprimirResultado(3, 4, function(x,y){return x- y})
 imprimirResultado(3 ,4, (x,y)=> x * y)
-imprimirResultado(10,2, function(x,y){return x * y})
+imprimirResultado(10 ,2 , function(x,y){return x * y})
 
 const pessoa ={
     nome: 'josuel',
