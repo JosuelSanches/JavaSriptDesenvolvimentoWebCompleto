95ca0bb94dac4b701f38abbc341bdab80ac24b30
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-06-17 17:50:31 -0300

    função anônima
    teste em função anônima

--- funcao/funcoesanonimas.js
+++ funcao/funcoesanonimas.js
@@ -8,4 +8,5 @@ const imprimirResultado = function(a, b, operacao = soma){
 imprimirResultado(3,4)
 imprimirResultado(3, 4, soma)
 imprimirResultado(3, 4, function(x,y){return x- y})
-imprimirResultado(6,2, (x,y)=> x * y)
+imprimirResultado(3 ,4, (x,y)=> x * y)
+imprimirResultado(100,2, function(x,y){return x/y})
