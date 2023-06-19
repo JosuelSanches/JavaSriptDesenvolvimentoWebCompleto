c1f660589b205f9bcb374c0793a6a4eab5882543
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-06-19 17:29:21 -0300

    Questao2
    Resolução, descobrindo o tipo de triangulo.

--- Exercicios/questao2.js
+++ Exercicios/questao2.js
@@ -8,3 +8,22 @@
  * e retorne a sua classificação quanto ao tamanho de seus lados.
  * (Neste exemplo deve-se abstrair as condições matemáticas de existência de um triangulo)
  */
+
+const tipoTriangulo = (a, b, c)=>{
+    if(!!Number.isInteger(a) && !!Number.isInteger(b) && !!Number.isInteger(c)){
+    if(a == b && b == c){
+        console.log(`O suposto triângulo formado pelos numeros ${a}, ${b} e ${c} é equilatero`)
+    }else if(a == b || a == c || c == b){
+        console.log(`O suposto triângulo formado pelos numeros ${a}, ${b} e ${c} é isósceles`)
+    }else if(a != b && a != c && b != c){
+        console.log(`O suposto triângulo formado pelos numeros ${a}, ${b} e ${c} é escaleno`)
+    }
+}else{
+        console.log(`Os valores são invalidos...`)
+    }
+}
+
+tipoTriangulo(2,2,2)
+tipoTriangulo(2,3,2)
+tipoTriangulo(1,2,3)
+tipoTriangulo(0, 'u', 8)
