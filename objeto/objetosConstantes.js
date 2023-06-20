e3477ba54429517c94deab4f501940f1d70336c2
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-06-20 17:25:22 -0300

    objetos contantes
    Por que um objeto atribuído à uma variável constante pode sofrer alteração?

---
+++ objeto/objetosConstantes.js
@@ -0,0 +1,17 @@
+/* A constante aponta para o endereco de memoria onde o objeto se encontra.
+Entao vc pode mudar o objeto, mas nao pode mudar o endereco de memoria,
+caso vc tente fazer com que essa constante aponte para outro objeto falhará miseralvelmente*/
+
+const pessoa = {nome: "Joao"}
+pessoa.nome = 'Pedro'
+console.log(pessoa)
+
+/**
+ * pessoa -> 423 -> {...}
+ * pessoa = {nome: 'ana'} // Isso nao vai funcionar porque a constrante
+ * ja aponta para outro endereco de memoria.
+ */
+
+Object.freeze(pessoa)
+pessoa.nome = 'Maria'
+console.log(pessoa.nome)
