fe30c0b0b27c1c28a41929d6fc6e4528e46e1419
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-06-21 15:24:45 -0300

    Questão4
    Divisão e resto da divisão.

---
+++ Exercicios/questao4.js
@@ -0,0 +1,8 @@
+/***
+ * Crie uma funcao que irá receber dois valores, o dividendo e o divisor.
+ * A função deverá imprimir o resultado e o resto da divisão destes dois valores.
+ */
+
+const divisaoEResto = (x,y)=> a = console.log(`${x} /${y} é igual a: ${Math.floor(x/y)} e sobra: ${x%y}`)
+
+divisaoEResto(21, 2)
