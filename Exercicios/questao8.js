03e5de0c40975661ebeb765fe0dd81f6d4eca259
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-06-22 15:43:05 -0300

    Questão 8
    Problema sobre a contagem de pontos, recordes e o pior jogo de basquete.

---
+++ Exercicios/questao8.js
@@ -0,0 +1,38 @@
+/**
+ *  Pedro joga N jogos de basquete por temporada. Para saber como está ele está progredindo, ele mantém
+registro de todos os as pontuações feitas por jogo. Após cada jogo ele anota no novo valor e confere se o
+mesmo é maior ou menor que seu melhor e pior desempenho. 
+Dada uma lista string = “pontuação1 pontuação2
+pontuação3 etc..”, 
+escreva uma função que ao recebê-la irá comparar os valores um a um e irá retornar um
+vetor com o número de vezes que ele bateu seu recorde de maior número de pontos e quando fez seu pior
+jogo. (Número do pior jogo).
+ */
+
+const point = (pontos)=>{
+    let recorde = 0
+    let pior = 0
+    let numRecorde = []
+    for(let i = 0; i <pontos.length; i++ ){
+        if(i == 0 || pontos[i] > recorde  ){
+            recorde=pontos[i]
+            numRecorde.push(pontos[i])
+            if(i == 0){
+                pior = pontos[i]
+            }
+        }else if(pontos[i] < pior){
+            pior = pontos[i]
+        }
+    }
+    return tabela={
+        pior,
+        numRecorde
+
+    }
+}
+
+let pontos = [10, 2, 30, 90, 5, 1, 3]
+console.log(pontos)
+let mostrar = point(pontos)
+console.log(`A sua pior pontuação foi ${mostrar.pior} e os seus recordes foram
+ ${mostrar.numRecorde} desde o primeiro jogo bateu o recorde ${mostrar.numRecorde.length} vezes.`)
