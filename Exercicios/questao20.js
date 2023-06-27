c8f0dd4173eed54b5f42415514230ac1fe0810e3
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-06-27 17:07:09 -0300

    Correção
    corrigindo numeração das questões e iniciando a analise 20 ª questão.

--- Exercicios/questao20.js
+++ Exercicios/questao20.js
@@ -1,52 +1,21 @@
 /**
-O cardápio de uma lanchonete é o seguinte:
+ Crie um programa para informar quais e quantas notas são necessárias para entregar o mínimo de cédulas
+para um determinado valor informado pelo usuário considerando notas de R$ 100, R$ 50, R$ 10 e R$ 5 e R$ 1.
 
-Código          Descrição do Produto                Preço
-100             Cachorro Quente                     R$ 3,00
-200             Hambúrguer Simples                  R$ 4,00
-300             Cheeseburguer                       R$ 5,50
-400             Bauru                               R$ 7,50
-500             Refrigerante                        R$ 3,50
-600             Suco                                R$ 2,80
-Implemente uma função que receba como parâmetros o código do item pedido, a quantidade
- e calcule o valor a ser pago por aquele lanche. 
-Considere que a cada execução somente será calculado um item. Use o
-comando switch. Crie um caso default para produto não existente.
+Seu programa deve mostrar apenas as notas utilizadas. 
+Por exemplo, ao solicitar R$18, o programa deve
+informar apenas a seguinte informação 
+(note que não foram exibidas informações sobre as demais cédulas): 1
+nota(s) de R$ 10. 1 nota(s) de R$ 5. 3 nota(s) de R$ 1.
 
  */
 
-const totalAPagar = (cod, quant)=>{
-    switch(cod){
-        case 100:
-                return `Voçê pediu CACHORRO QUENTE, na quantidade de  ${quant}, custará R$ ${(quant * 3.00).toFixed(2)}.`
-                break
-    
-        case 200:
-                return `Voçê pediu HAMBURGUER SIMPLES, na quantidade de  ${quant}, custará R$ ${(quant * 4.00).toFixed(2)}.`
-                break
-    
-        case 300:
-                return `Voçê pediu CHEESEBURGUER, na quantidade de  ${quant}, custará R$ ${(quant * 5.50).toFixed(2)}.`
-                break
-    
-        case 400:
-                return `Voçê pediu BAURU, na quantidade de  ${quant}, custará R$ ${(quant * 7.50).toFixed(2)}.`
-                break
-    
-        case 500:
-                return `Voçê pediu REFRIGERANTE, na quantidade de  ${quant}, custará R$ ${(quant * 3.50).toFixed(2)}.`
-                break
-    
-        case 600:
-                return `Voçê pediu SUCO, na quantidade de  ${quant}, custará R$ ${(quant * 2.80).toFixed(2)}.`
-                break
-    } return `Não vendemos isto aqui! valor invalido.`
+const dinheiroEspecie = (valor)=>{
+    let notas1 = 0
+    let notas5 = 0 
+    let notas10 = 0
+    let notas50 = 0
+    let notas100 = 0
 
+    
 }
-console.log(totalAPagar(100, 3))
-console.log(totalAPagar(200, 3))
-console.log(totalAPagar(300, 3))
-console.log(totalAPagar(400, 3))
-console.log(totalAPagar(500, 3))
-console.log(totalAPagar(600, 3))
-console.log(totalAPagar(700, 3))
