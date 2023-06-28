e989ffd0e1425b2940a995d3fc8d46f7f47e9760
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-06-28 11:39:22 -0300

    Questão20
    Problema envolvendo saque de dinheiro.

--- Exercicios/questao20.js
+++ Exercicios/questao20.js
@@ -17,5 +17,69 @@ const dinheiroEspecie = (valor)=>{
     let notas50 = 0
     let notas100 = 0
 
+    const notaMaiorQue = (valor) =>{
+        if(valor >= 100){
+            return 100
+        }else if(valor >= 50){
+            return 50
+        }else if(valor >= 10){
+            return 10
+        }else if(valor >= 5){
+            return 5
+        }else if(valor >= 1){
+            return 1
+        }
+    }
+
+    let valorNota = notaMaiorQue(valor)
+    while (valor >= valorNota){
+        switch(valorNota){
+            case 100: 
+                valor -= 100
+                notas100 ++
+                break
+            case 50: 
+                valor -= 50
+                notas50 ++
+                break
+            case 10: 
+                valor -= 10
+                notas10 ++
+                break
+            case 5: 
+                valor -= 5
+                notas5 ++
+                break
+            case 1: 
+                valor -= 1
+                notas1 ++
+                break
+        }
+        valorNota = notaMaiorQue(valor)
+    }
+
+    let resultado = (notas1, notas5, notas10, notas50, notas100)=>{
+        let print = ``
+        if (notas100 > 0){
+            print += `São ${notas100} nota(s) de 100.\n`
+        }if (notas50 > 0){
+            print += `São ${notas50} nota(s) de 50.\n`
+        }if (notas10 > 0){
+            print += `São ${notas10} nota(s) de 10.\n`
+        }if (notas5 > 0){
+            print += `São ${notas5} nota(s) de 5.\n`
+        }
+        if (notas1 > 0){
+            print += `São ${notas1} nota(s) de 1.\n`
+        }
+
+        return print
+
+    }
+
+    return resultado(notas1, notas5, notas10, notas50, notas100)
+
     
 }
+
+console.log(dinheiroEspecie(476.90))
