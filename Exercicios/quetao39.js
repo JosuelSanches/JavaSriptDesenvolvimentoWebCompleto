e292bd50d49adf10cdd894f5461ec25304ed82f3
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-07-06 14:43:35 -0300

    questão 39
    Adequação da resolução ao comando.

--- Exercicios/quetao39.js
+++ Exercicios/quetao39.js
@@ -2,20 +2,20 @@
 primeiro elemento do vetorA passe a ser o primeiro elemento do vetorB e vice versa e assim sucessivamente.
 Faça a troca sem utilizar uma variável auxiliar.
  */
-let vet1 = [1, 3, 5, 7, 8]
-let vet2 = [0, 5, 2, 9, 10]
+let vetorA = [1, 3, 5, 7, 8]
+let vetorB = [0, 5, 2, 9, 10]
 
 const troca = (num, num2)=>{
     if(num.length == num2.length){
-    for(i =0; i < vet1.length; i++){
+    for(i =0; i < num.length; i++){
             num[i] = num[i]+ num2[i]
             num2[i] = num[i] - num2[i]
             num[i] = num[i] - num2[i]
-    }console.log(vet1, vet2)
+    }console.log(vetorA, vetorB)
     }else{
         console.log("Os vetores são de tamanhos diferentes, então a troca não será possivel.")
     }
 }
 
-console.log(vet1, vet2)
-troca(vet1, vet2)
+console.log(vetorA, vetorB)
+troca(vetorA, vetorB)
