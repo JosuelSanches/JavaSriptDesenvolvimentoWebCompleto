e3e57421126e8472d745d8fef85a467e6b982c10
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-07-06 16:47:34 -0300

    questão 5
    Crie uma função que receba dois números e retorne se o primeiro é maior ou igual ao segundo.

---
+++ Exercicios/ExerciciosFundamentosJavaScript/questao5.js
@@ -0,0 +1,5 @@
+/*Crie uma função que receba dois números e retorne se o primeiro é maior ou igual ao segundo. */
+const maiorOuIgual = (num1, num2)=> num1 >= num2
+let num1 = 10
+let num2 = 90
+console.log(`O número ${num1} é maior ou igual ao número ${num2}: ${maiorOuIgual(num1, num2)}`)
