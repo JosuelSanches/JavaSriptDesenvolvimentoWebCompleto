b834ea4ad5c5222b6743bc8a7b71e676626340a3
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-07-10 14:19:00 -0300

    Acessando Módulos
    Como acessar módulos em JS.

---
+++ node/pastaA/pastaB/acessarModulo.js
@@ -0,0 +1,13 @@
+const moduloA = require('../../moduloA')
+//const moduloA = require('D:\Sanches\JavaScript-definitive-learning\JavaSriptDesenvolvimentoWebCompleto\node\moduloA.js')
+console.log(moduloA.ola)
+
+
+/*
+const http = require('http')
+http.createServer((req, res)=>{
+    res.write('Bom dia!')
+    res.end()
+}), listen(8080)*/
+const c = require('./pastaC')
+console.log(c.ola)
