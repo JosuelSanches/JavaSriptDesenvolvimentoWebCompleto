3515e2e298e894172d993bf8168ce6e381b237e7
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-07-10 15:26:08 -0300

    Encontrando um funcionário.
    Encontrando a funcionaria chinesa com o menor salário.

--- node/funcionarios/funcionarios.js
+++ node/funcionarios/funcionarios.js
@@ -3,5 +3,18 @@ const axios = require('axios')
 
 axios.get(url).then(response =>{
     const funcionarios = response.data
-    console.log(funcionarios)
+    //console.log(funcionarios)
+    /*Encontrar a mulher chinesa que ganha o menor salario */
+    let menorSalChinesa = {salario: 1000000}
+    const chinesaMenorSal = (pessoa) =>{
+        for(let i in pessoa){
+        if (pessoa[i].pais == 'China' && pessoa[i].genero == 'F'){
+            if(pessoa[i].salario < menorSalChinesa.salario){
+                menorSalChinesa = pessoa[i]
+            }
+        }
+    }
+    }
+    chinesaMenorSal(funcionarios)
+    console.log(menorSalChinesa)
 })
