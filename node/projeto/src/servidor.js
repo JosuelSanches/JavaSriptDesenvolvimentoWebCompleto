468ff777022d742c588899f1b7ab5b6eec3b9b9c
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-07-12 15:55:07 -0300

    parte 2 API
    Criando um arquivo js para gerenciamento de dados.

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -7,8 +7,19 @@ app.get('/produtos', (req, res, next)=>{
     console.log('Middleware 1...')
     next()
 })
+const bancoDeDados = require('./bancoDeDados')
 app.get('/produtos', (req, res, next)=>{
-    res.send({nome: 'JBL Boobox 3', preco: 2500.00}) // Converte para JSON
+    res.send(bancoDeDados.getProdutos)
+})
+app.get('/produto/:id', (req, res, next)=>{
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+app.post('/produtos', (req, res, next)=>{
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
 })
 
 app.listen(porta, ()=>{
