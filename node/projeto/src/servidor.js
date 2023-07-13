900c6b8b3e36a6ab9ea2fc691f8421e9ef482586
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-07-13 13:45:07 -0300

    api projeto
    modificações

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -7,15 +7,27 @@ app.get('/produtos', (req, res, next)=>{
     console.log('Middleware 1...')
     next()
 })
+const bodyParser = require('body-parser')
 const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get('/produtos', (req, res, next)=>{
-    res.send(bancoDeDados.getProdutos)
+    res.send(bancoDeDados.getProdutos())
 })
 app.get('/produto/:id', (req, res, next)=>{
     res.send(bancoDeDados.getProduto(req.params.id))
 })
 app.post('/produtos', (req, res, next)=>{
     const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
+})
+app.put('/produtos/:id', (req, res, next)=>{
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
         nome: req.body.nome,
         preco: req.body.preco
     })
