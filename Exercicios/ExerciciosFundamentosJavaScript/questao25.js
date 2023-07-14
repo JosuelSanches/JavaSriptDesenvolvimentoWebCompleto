a37baba8357c5a9628f6026b21aaa8e3a3811c6e
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-07-14 17:30:04 -0300

    procurando palavra em outras
    A fim de criar um mecanismo de busca para sua aplicacao, voce precisa iniciar criando uma funcao
    para identificar palavras semelhantes.
    Escreva esta funcao que recebe como primeiro parametro uma palavra e, como segundo parametro,
    um array de strings. A funcao devera filtrar as palavras do array que contem nelas a
    string do primeiro parametro.

---
+++ Exercicios/ExerciciosFundamentosJavaScript/questao25.js
@@ -0,0 +1,23 @@
+/*A fim de criar um mecanismo de busca para sua aplicacao, voce precisa iniciar criando uma funcao
+para identificar palavras semelhantes.
+Escreva esta funcao que recebe como primeiro parametro uma palavra e, como segundo parametro, 
+um array de strings. A funcao devera filtrar as palavras do array que contem nelas a 
+string do primeiro parametro. */
+const buscaPalavras = (busca, em)=>{
+    let igual = 0
+    let palavras =[]
+    for(let j in em){
+        for(let i in busca){
+            if(busca[i] == em[j][i]){
+                igual += 1
+                if(igual == busca.length){
+                    palavras.push(em[j])
+                    igual = 0
+                }
+            }
+
+        }
+    } return palavras
+}
+let palavras = [`programacao`,`mobile`, `proficional`, `pro` ]
+console.log(buscaPalavras(`pro`, palavras))
