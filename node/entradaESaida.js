b6adc38cf6e12c21d937b154676182d49999e84c
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-07-14 14:05:06 -0300

    Trying to print
    Trying to print the real expected output

--- node/entradaESaida.js
+++ node/entradaESaida.js
@@ -7,13 +7,17 @@ if (anonimo){
     process.stdout.write('Informe o seu nome: ')
     process.stdin.on('data', data =>{
         let nome = data.toString()
+        //nome = nome.slice(0,-1)
         nome = nome.slice(0,-1)
 
         process.stdout.write("Fala") 
 
         process.stdout.write(nome)
 
+        //process.stdout.write("!!")
         process.stdout.write("!!")
+        //process.stdout.write(`Fala ${nome}!!`)
+        //console.log(`Fala ${noem}!`)
         process.exit()
     })
 }
