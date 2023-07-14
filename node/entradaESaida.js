4ebdf61dd3452f0e6c59271aefafff25accba773
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-07-14 14:28:30 -0300

    more trying to print
    trying to print the expected result

--- node/entradaESaida.js
+++ node/entradaESaida.js
@@ -6,17 +6,19 @@ if (anonimo){
 }else{
     process.stdout.write('Informe o seu nome: ')
     process.stdin.on('data', data =>{
-        let nome = data.toString()
+        const nome = data.toString().replace('\n','')
+        //let nome = data.toString()
         //nome = nome.slice(0,-1)
-        nome = nome.slice(0,-1)
+        //nome = nome.substring(0,nome.length-1)
+        //console.log(`Fala ${nome}!!`)
 
-        process.stdout.write("Fala") 
+        //process.stdout.write("Fala") 
 
-        process.stdout.write(nome)
+        //process.stdout.write(nome)
 
         //process.stdout.write("!!")
-        process.stdout.write("!!")
-        //process.stdout.write(`Fala ${nome}!!`)
+        //process.stdout.write("!!")
+        process.stdout.write(`Fala ${nome}!!`)
         //console.log(`Fala ${noem}!`)
         process.exit()
     })
