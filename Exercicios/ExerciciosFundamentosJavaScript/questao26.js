c0d00c47e755f93b401d3f75ba05cb32ad9e6aab
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-07-15 16:44:26 -0300

    Retirando vogais de uma string
    Retirando vogais de uma string versao reduzida.

--- Exercicios/ExerciciosFundamentosJavaScript/questao26.js
+++ Exercicios/ExerciciosFundamentosJavaScript/questao26.js
@@ -2,11 +2,7 @@
 e retorne essa string somente com as consoantes, ou seja, sem as vogais */
 const soConsoantes = (palavra)=>{
     for(let i in palavra){
-        palavra = palavra.replace("a","")
-        palavra = palavra.replace("e","")
-        palavra = palavra.replace("i","")
-        palavra = palavra.replace("o","")
-        palavra = palavra.replace("u","")
+        palavra = palavra.replace(/[aeiou]/ig,"")
     }return palavra
     }
 
