7894ee630f276aa487953ad8f652a045589c2eef
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-07-16 14:55:46 -0300

    Questao 28
    Elabore uma funcao que receba dois parametros:
    o primeiro e um array de numeros e o segundo e um numero
    que especifica a quantidade de digitos.
    Essa fumcao devera retornar apenas aqueles numeros
    do array que tem a quantidade de digitos indicadas pelo segundo parametro.

---
+++ Exercicios/ExerciciosFundamentosJavaScript/questao28.js
@@ -0,0 +1,19 @@
+/*Elabore uma funcao que receba dois parametros:
+o primeiro e um array de numeros e o segundo e um numero
+que especifica a quantidade de digitos.
+Essa fumcao devera retornar apenas aqueles numeros 
+do array que tem a quantidade de digitos indicadas pelo segundo parametro. */
+let numeros = [1, 19, 29, 9, 0, 711, 28, 200]
+
+const quantDig = (nums, num)=>{
+    let resultado =[]
+    for(let n of nums){ 
+        if((String(n).length) === num){
+            resultado.push(n)
+        }
+
+    }
+
+    return resultado
+} 
+console.log(quantDig(numeros, 3))
