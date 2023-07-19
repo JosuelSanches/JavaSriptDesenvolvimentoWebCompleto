b9980bfc7cc071780d41ae38813756b1a5af916a
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-07-19 14:39:17 -0300

    usando o promise
    formatado.

--- controle/esNext/usandoPromises.js
+++ controle/esNext/usandoPromises.js
@@ -3,17 +3,17 @@ const http = require('http')
 
 const getTurma = letra => {
     const url = `http://files.cod3r.com.br/curso-js/turma${letra.toUpperCase()}.json`
-    return new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject) => {
         http.get(url, res => {
             let resultado = ''
-    
-            res.on('data', dados =>{
+
+            res.on('data', dados => {
                 resultado += dados
             })
             res.on('end', () => {
-                try{
+                try {
                     resolve(JSON.parse(resultado))
-                }catch(e){
+                } catch (e) {
                     reject(e)
                 }
             })
@@ -37,6 +37,6 @@ Promise.all([getTurma(`A`), getTurma(`B`), getTurma(`C`)])
     .then(turmas => [].concat(...turmas))
     .then(alunos => alunos.map(aluno => aluno.nome))
     .then(nomes => console.log(nomes))
-    .catch(e=> console.log(e.message))
+    .catch(e => console.log(e.message))
 
 getTurma(`D`).catch(e => console.log(e.message))
