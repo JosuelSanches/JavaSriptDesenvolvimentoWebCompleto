e9e2dec1bb837d85a06bfcfe2512ec37e79befa6
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-07-22 19:51:13 -0300

    questao 30
    Elabore uma funcao que recebe um objeto com estudantes e suas notas.
    As notas de cada estudando estarão num array, conforme exemplo abaixo.
    Voce deverá calcular a media da nota de cada aluno e retorna um objeto
    com os atributos nome e media, que indica o aluno que teve o melhor desempenho
    nas notas.

---
+++ Exercicios/ExerciciosFundamentosJavaScript/questao30.js
@@ -0,0 +1,5 @@
+/*Elabore uma funcao que recebe um objeto com estudantes e suas notas.
+As notas de cada estudando estarão num array, conforme exemplo abaixo.
+Voce deverá calcular a media da nota de cada aluno e retorna um objeto
+com os atributos nome e media, que indica o aluno que teve o melhor desempenho
+nas notas. */
