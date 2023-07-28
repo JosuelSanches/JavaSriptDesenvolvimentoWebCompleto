f66f63d054814e2192cc832f58b38ceb166916d9
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-07-28 15:34:13 -0300

    formularios.
    Criando diferentes tipos de formularios em html.

--- exerciciosWeb/html/exercicios/form/server.js
+++ exerciciosWeb/html/exercicios/form/server.js
@@ -4,8 +4,8 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.post('/usuario',(req, resp) => {
+app.post('/usuarios',(req, resp) => {
     console.log(req.body)
-    resp.send(`<h1>Parabens</h1>`)
+    resp.send('<h1>Parabens</h1>')
 })
-app.listen(3000)
+app.listen(3003)
