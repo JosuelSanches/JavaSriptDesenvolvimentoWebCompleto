62718efa84039cdae2eeefe55f5219ef8b623bf1
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-07-29 16:07:28 -0300

    questao 30
    questao 30

--- Exercicios/ExerciciosFundamentosJavaScript/questao30.js
+++ Exercicios/ExerciciosFundamentosJavaScript/questao30.js
@@ -23,7 +23,7 @@ const soma = array => array.reduce((acumulador, atual)=> acumulador + atual, 0)
 const media = array => soma(array)/ array.legth
 
 function receberMelhorEstudante(estudantes){
-    const estudantesComMedias = Objeto.entries(estudantes).map(estudante => {
+    const estudantesComMedias = Object.entries(estudantes).map(estudante => {
         const chave = 0,
               valor = 1
         return{nome : estudante[chave], media: media(estudante[valor])}
