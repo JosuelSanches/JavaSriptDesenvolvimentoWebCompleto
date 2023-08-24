859b6377669e19c1ae93fc339e032da5f67843ce
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-08-24 16:45:20 -0300

    flappy bird
    flappy bird script

--- exerciciosWeb/dom/js/flappy.js
+++ exerciciosWeb/dom/js/flappy.js
@@ -20,7 +20,7 @@ function Barreira(reversa = false) {
 // document.querySelector('[wm-flappy]').appendChild(b.elemento)
 
 function ParDeBarreiras(altura, abertura, x){
-    this.element = novoElemento('div', 'par-de-barreiras')
+    this.elemento = novoElemento('div', 'par-de-barreiras')
 
     this.superior = new Barreira(true)
     this.inferior = new Barreira(false)
@@ -43,5 +43,39 @@ function ParDeBarreiras(altura, abertura, x){
     this.setX(x)
 }
 
-const b = new ParDeBarreiras(700, 200, 400)
-document.querySelector('[wm-flappy]').appendChild(b.elemento)
+// const b = new ParDeBarreiras(500, 200, 600)
+// document.querySelector('[wm-flappy]').appendChild(b.elemento)
+
+function Barreiras(altura, largura, abertura, espaco, notificarPontos){
+    this.pares = [
+        new ParDeBarreiras(altura, abertura, largura),
+        new ParDeBarreiras(altura, abertura, largura + espaco),
+        new ParDeBarreiras(altura, abertura, largura + espaco * 2),
+        new ParDeBarreiras(altura, abertura, largura + espaco * 3)
+    ]
+
+    const deslocamento = 3
+    this.animar = () =>{
+        this.pares.forEach(par => {
+            par.setX(bar.getX() - deslocamento)
+
+            // Quando o elemento sair da area do jogo
+            if(par.getX() < -par.getLargura()){
+                par.setX(par.getX() + espaco * this.pares.length)
+                par.sortearAbertura()
+            }
+
+            const meio = largura / 2
+            const cruzouOMeio = par.getX() + deslocamento >= meio
+                && par.getX() < meio
+            if(cruzouOMeio) notificarPontos()
+        })
+    }
+}
+
+const barreiras = new Barreiras(700, 1200, 200, 400)
+const areaDoJogo = document.querySelector('[wm-flappy]')
+barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+setInterval(()=> {
+    barreiras.animar()
+}, 20)
