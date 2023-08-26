15df85f9641ae6b1caf456f55e3e4b997b51f93f
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-08-26 16:17:09 -0300

    flappy bird
    Correcao na funcao (estao sobrepostos).

--- exerciciosWeb/dom/js/flappy.js
+++ exerciciosWeb/dom/js/flappy.js
@@ -123,7 +123,7 @@ function estaoSobrepostos(elementoA, elementoB){
     const a = elementoA.getBoundingClientRect()
     const b = elementoB.getBoundingClientRect()
 
-    const horizontal = a.left + a.width >= a.left
+    const horizontal = a.left + a.width >= b.left
         && b.left + b.width >= a.left
     const vertical = a.top + a.height >= b.top
         && b.top + b.height >= a.top
