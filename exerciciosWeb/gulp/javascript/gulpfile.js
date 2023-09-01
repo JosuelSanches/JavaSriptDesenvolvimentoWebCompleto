b15adea7b853a2c5c0f5920f15ed5b0440ea0ba4
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-09-01 16:40:21 -0300

    gulp
    utilizando gulp e babel.

---
+++ exerciciosWeb/gulp/javascript/gulpfile.js
@@ -0,0 +1,14 @@
+const { series }= require('gulp')
+const gulp = require('gulp')
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify')
+const babel = require('gulp-babel')
+
+function padrao(cb){
+    gulp.src('src/**/*.js')
+        .bipe(babel({
+            coments: false,
+            presents: ["env"]
+        }))
+    return cb()
+}
