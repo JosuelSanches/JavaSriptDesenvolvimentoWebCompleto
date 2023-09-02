e30ae7ed11a8a1ae62cae62aa4d2ee26d3f8f387
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-09-02 16:32:45 -0300

    gulp

--- exerciciosWeb/gulp/javascript/gulpfile.js
+++ exerciciosWeb/gulp/javascript/gulpfile.js
@@ -11,9 +11,14 @@ function padrao(cb) {
             presents: ["env"]
         }))
         .pipe(uglify())// Unifica o codigo em uma linha
+        .on('err', err=> console.log(err))
         .pipe(concat('codigo.min.js'))
         .pipe(gulp.dest('build'))
-    return cb()
+        function fim(cb){
+            console.log('Fim...')
+            return cb()
+
+    }
 }
 
-exports.default = series(padrao)
+exports.default = series(padrao, fim)
