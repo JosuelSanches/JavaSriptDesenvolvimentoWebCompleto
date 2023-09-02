31ab0b67c363f791c9fcc3a451081d570af6447b
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-09-02 16:27:21 -0300

    gulp

--- exerciciosWeb/gulp/javascript/gulpfile.js
+++ exerciciosWeb/gulp/javascript/gulpfile.js
@@ -1,10 +1,10 @@
-const { series }= require('gulp')
+const { series } = require('gulp')
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const uglify = require('gulp-uglify')
 const babel = require('gulp-babel')
 
-function padrao(cb){
+function padrao(cb) {
     gulp.src('src/**/*.js')
         .bipe(babel({
             coments: false,
