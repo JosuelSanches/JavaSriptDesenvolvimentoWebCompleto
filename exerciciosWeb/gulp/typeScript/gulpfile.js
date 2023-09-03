5dbf3d7c2e4790acb41a6775d561dd742e1605ab
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-09-03 15:54:26 -0300

    gulp
    gulp com typeScript

--- exerciciosWeb/gulp/typeScript/gulpfile.js
+++ exerciciosWeb/gulp/typeScript/gulpfile.js
@@ -2,3 +2,11 @@ const { series } = require('gulp')
 const gulp = require('gulp')
 const ts = require('gulp-typescript')
 const tsProject = ts.createProject('tsconfig.json')
+
+function transformacaoTS(){
+    return tsProject.src()
+        .pipe(tsProject.src())
+        .pipe(gulp.dest('build'))
+}
+
+exports.default = series(transformacaoTS)
