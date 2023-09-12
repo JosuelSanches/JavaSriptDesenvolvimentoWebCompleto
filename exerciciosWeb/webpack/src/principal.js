7b6cbc441307a1750ad5efe890ca396545daf051
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-09-12 14:13:50 -0300

    Update principal.js
    comentario

--- exerciciosWeb/webpack/src/principal.js
+++ exerciciosWeb/webpack/src/principal.js
@@ -1,6 +1,7 @@
 // const Pessoa = require('./pessoa')
 import Pessoa from './pessoa'
 import './assets'
+const autor = 'Josuel de souza sanches'
 
 const atendente = new Pessoa
 console.log(atendente.cumprimentar())
