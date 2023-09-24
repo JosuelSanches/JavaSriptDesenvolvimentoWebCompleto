29fb2096a8a7305754227f35681b1f18664beeaf
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-09-24 16:24:10 -0300

    projeto galeria
    Removendo classe para que no momento em que uma imagem é escondida não fique uma espaço vazio no lugar.

--- exerciciosWeb/bootstrap/projeto-galeria/src/js/plugins/cityButtons.js
+++ exerciciosWeb/bootstrap/projeto-galeria/src/js/plugins/cityButtons.js
@@ -4,47 +4,47 @@ import { onLoadHtmlSuccess } from '../core/includes'
 
 const duration = 600
 
-function filterByCity(city){
-    $(`[wm-city]`).each(function(i,e){
+function filterByCity(city) {
+    $('[wm-city]').each(function (i, e) {
         const isTarget = $(this).attr('wm-city') === city
             || city === null
-        if(isTarget){
+        if (isTarget) {
             $(this).parent().removeClass('d-none')
             $(this).fadeIn(duration)
-        }else{
-            $(this).fadeOut(duration), ()=>{
+        } else {
+            $(this).fadeOut(duration, () => {
                 $(this).parent().addClass('d-none')
-            }
+            })
         }
     })
 }
 
-$.fn.cityButtons = function (){
-    
-const cities = new Set
-$('[wm-city]').each(function(i, e){
-    cities.add($(e).attr('wm-city'))
-})
+$.fn.cityButtons = function () {
 
-const btns = Array.from(cities).map(city => {
-    const btn = $('<button>')
-        .addClass(['btn', 'btn-info']).html(city)
-    btn.click(e => filterByCity(city))
-    return btn
-})
+    const cities = new Set
+    $('[wm-city]').each(function (i, e) {
+        cities.add($(e).attr('wm-city'))
+    })
 
-const btnAll = $('<button>')
-    .addClass(['btn', 'btn-info', 'active']).html('Todas')
-btnAll.click(e => filterByCity(null))
-btns.push(btnAll)
+    const btns = Array.from(cities).map(city => {
+        const btn = $('<button>')
+            .addClass(['btn', 'btn-info']).html(city)
+        btn.click(e => filterByCity(city))
+        return btn
+    })
 
-const btnGroup = $('<div>').addClass(['btn-group'])
-btnGroup.append(btns)
+    const btnAll = $('<button>')
+        .addClass(['btn', 'btn-info', 'active']).html('Todas')
+    btnAll.click(e => filterByCity(null))
+    btns.push(btnAll)
 
-this.html(btnGroup)
-return this
+    const btnGroup = $('<div>').addClass(['btn-group'])
+    btnGroup.append(btns)
+
+    this.html(btnGroup)
+    return this
 }
 
-onLoadHtmlSuccess(function(){
+onLoadHtmlSuccess(function () {
     $('[wm-city-buttons]').cityButtons()
 })
