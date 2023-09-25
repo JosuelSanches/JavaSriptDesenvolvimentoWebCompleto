1d9378da359c313ccf987c692aa7ba5a1a7b61aa
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-09-25 16:39:29 -0300

    Primeiro React
    Componente função.

--- exerciciosWeb/react/exercicios/src/index.js
+++ exerciciosWeb/react/exercicios/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-const elemento = <h2>React</h2>
-ReactDOM.render(elemento, document.getElementById('root'))
+
+import Primeiro from './componentes/primeiro'
+ReactDOM.render(< Primeiro/>, document.getElementById('root'))
