b8dcae4fcb9f35dd98a17019dbe4323251c4f54e
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-09-26 14:01:33 -0300

    Exercicios React
    export e import.

--- exerciciosWeb/react/exercicios/src/index.js
+++ exerciciosWeb/react/exercicios/src/index.js
@@ -1,5 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+//import { BoaTarde, BoaNoite } from './componentes/multiplos'
+import NomeQEuQuiser from './componentes/multiplos'
+
 import BomDia from './componentes/BomDia'
-ReactDOM.render(<BomDia nome="Josuel Sanches" />, document.getElementById('root'))
+    ReactDOM.render(
+        <div>
+            <BomDia nome="Carlos Almeida" />
+            <NomeQEuQuiser.BoaTarde nome="Bruna Souza"/>
+            <NomeQEuQuiser.BoaNoite nome="Joao Benute"/>
+        </div>
+    , document.getElementById('root'))
