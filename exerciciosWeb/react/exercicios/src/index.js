6194eb248bb7345c74334f07e9a9d684fa6d869a
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-09-26 15:02:45 -0300

    Exercicios React
    Componentes de classe.

--- exerciciosWeb/react/exercicios/src/index.js
+++ exerciciosWeb/react/exercicios/src/index.js
@@ -2,13 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 //import { BoaTarde, BoaNoite } from './componentes/multiplos'
-import NomeQEuQuiser from './componentes/multiplos'
+//import NomeQEuQuiser from './componentes/multiplos'
 
-import BomDia from './componentes/BomDia'
+import Saudacao from './componentes/saudacao'
+
+//import BomDia from './componentes/BomDia'
     ReactDOM.render(
-        <div>
+        /*<div>
             <BomDia nome="Carlos Almeida" />
             <NomeQEuQuiser.BoaTarde nome="Bruna Souza"/>
             <NomeQEuQuiser.BoaNoite nome="Joao Benute"/>
-        </div>
+            </div>*/
+
+            <Saudacao tipo="Bom dia" nome="Antonio de Almeida"/>
     , document.getElementById('root'))
