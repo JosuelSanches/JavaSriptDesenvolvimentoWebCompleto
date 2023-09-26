05190f5f0a0a4cc44a9fc965cfc651bca5f5fa56
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-09-26 16:09:42 -0300

    Exercícios React
    Componentes: Pai e filho.

--- exerciciosWeb/react/exercicios/src/index.js
+++ exerciciosWeb/react/exercicios/src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom'
 //import { BoaTarde, BoaNoite } from './componentes/multiplos'
 //import NomeQEuQuiser from './componentes/multiplos'
 
-import Saudacao from './componentes/saudacao'
+//import Saudacao from './componentes/saudacao'
+import Pai from './componentes/Pai'
+import { divide } from 'lodash'
 
 //import BomDia from './componentes/BomDia'
     ReactDOM.render(
@@ -12,7 +14,15 @@ import Saudacao from './componentes/saudacao'
             <BomDia nome="Carlos Almeida" />
             <NomeQEuQuiser.BoaTarde nome="Bruna Souza"/>
             <NomeQEuQuiser.BoaNoite nome="Joao Benute"/>
-            </div>*/
-
+            </div>
+            
             <Saudacao tipo="Bom dia" nome="Antonio de Almeida"/>
+            */
+           <div>
+                <Pai nome="Paulo" sobrenome="Silva"/>
+                {/*Assim é feito o comentario
+                Como passo os componentes filhos aqui?
+                */}
+
+           </div>
     , document.getElementById('root'))
