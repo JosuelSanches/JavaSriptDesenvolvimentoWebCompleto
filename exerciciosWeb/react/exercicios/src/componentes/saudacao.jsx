562b787a6b50410273497bc80a6fece4a09dc026
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-09-26 15:29:04 -0300

    Exercicios React
    Componentes de classe.

--- exerciciosWeb/react/exercicios/src/componentes/saudacao.jsx
+++ exerciciosWeb/react/exercicios/src/componentes/saudacao.jsx
@@ -5,6 +5,13 @@ export default class Saudacao extends Component{
         tipo: this.props.tipo,
         nome: this.props.nome
     }
+    
+    constructor(props){
+        super(props)
+
+        this.setTipo = this.setTipo.bind(this)
+        //this.setNome = this.setNome.bind(this)
+    }
     setTipo(e){
         this.setState({ tipo: e.target.value })
     }
@@ -18,7 +25,7 @@ export default class Saudacao extends Component{
                 <h1>{tipo} {nome}!</h1>
                 <hr />
                 <input type="text" placeholder="Tipo..." 
-                    value={tipo} onChange={e => this.setTipo(e)}/>
+                    value={tipo} onChange={this.setTipo}/>
                 <input type="text" placeholder="Nome..."
                      value={nome} onChange={e=> this.setNome(e)}/>
             </div>
