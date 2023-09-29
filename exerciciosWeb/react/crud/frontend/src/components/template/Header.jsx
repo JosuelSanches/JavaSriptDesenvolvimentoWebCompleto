88fdd12dcd12d734ae61e8c80bab43cc176bf5ee
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-09-29 16:24:56 -0300

    CRUD
    Componente de Cabeecalho.

--- exerciciosWeb/react/crud/frontend/src/components/template/Header.jsx
+++ exerciciosWeb/react/crud/frontend/src/components/template/Header.jsx
@@ -2,6 +2,6 @@ import './Header.css'
 import React from 'react'
 
 export default props => 
-    <header className="header">
+    <header className="header d-none d-sm-flex">
         Cabecalho
     </header>
