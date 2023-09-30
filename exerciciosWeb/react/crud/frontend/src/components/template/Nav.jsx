181f6c1ff75361b4172159236158114c359aea9a
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-09-30 15:44:53 -0300

    CRUD
    Componente Navegação.

--- exerciciosWeb/react/crud/frontend/src/components/template/Nav.jsx
+++ exerciciosWeb/react/crud/frontend/src/components/template/Nav.jsx
@@ -3,5 +3,13 @@ import React from 'react'
 
 export default props => 
     <aside className="menu-area">
-        Navegacao
+        <nav className='menu'>
+            {/*Refatorar depois para ter o mesmo esquema usado no Header */}
+            <a href='#/'>
+                <i className='fa fa-home'></i>Início
+            </a>
+            <a href='#/users'>
+                <i className='fa fa-users'></i>Usuarios
+            </a>
+        </nav>
     </aside>
