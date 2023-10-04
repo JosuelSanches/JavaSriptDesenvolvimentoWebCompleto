4ec59bf406b30e1579fbb52ce3b831e0f9af1292
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-10-04 16:48:30 -0300

    Next.js & React
    Componente Layout.

--- exerciciosWeb/react/aulao_nextJS/fundamentos/src/pages/estiloso.jsx
+++ exerciciosWeb/react/aulao_nextJS/fundamentos/src/pages/estiloso.jsx
@@ -1,10 +1,13 @@
 import styles from '../styles/Estiloso.module.css'
+import Layout from '../components/Layout'
 import Link from 'next/link'
 
 export default function Estiloso(){
     return(
-        <div className={styles.roxo}><h1>Estilo usando CSS modulos </h1>
-        <Link href="/">Voltar</Link>
+        <Layout titulo="Exemplo de CSS Modularizado">
+        <div className={styles.roxo}>
+            <h1>Estilo usando CSS modulos </h1>
         </div>
+        </Layout>
     )
 }
