efefde0d7e62ea288c36c10056f3abad9e33aa67
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-10-04 15:44:42 -0300

    Next.js & React
    Usando componentes.

---
+++ exerciciosWeb/react/aulao_nextJS/fundamentos/src/pages/exemplo.jsx
@@ -0,0 +1,11 @@
+import Cabecalho from "@/components/Cabecalho";
+
+export default function exemplo(){
+    return(
+        <>
+            <Cabecalho/>
+            <Cabecalho/>
+
+        </>
+    )
+}
