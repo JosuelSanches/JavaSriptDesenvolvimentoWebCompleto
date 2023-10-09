d0cc2df4904c60535538e99c0b47a5ba2a5f5851
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-10-09 16:52:14 -0300

    Next.js & React
    Componentes Layout e Titulo.

--- exerciciosWeb/react/next-crud/tailwind.config.js
+++ exerciciosWeb/react/next-crud/tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-  './src/pages/index.tsx'],
+  './src/pages/index.tsx',
+  './src/components/Titulo.tsx',
+  './src/components/Layout.tsx'],
   theme: {
     extend: {},
   },
