ae9114304dd96b1ff71356f8a82c9659fe2e7d15
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-10-13 14:30:17 -0300

    Next.js & React
    Repositorio de clientes

--- exerciciosWeb/react/next-crud/src/backend/db/ColecaoCliente.ts
+++ exerciciosWeb/react/next-crud/src/backend/db/ColecaoCliente.ts
@@ -1,23 +1,41 @@
+import firebase from "../config";
+import Cliente from "../../core/Cliente";
 import ClienteRepositorio from "../../core/ClienteRepositorio";
-import Cliente from "../../core/Cliente"
 
 export default class ColecaoCliente implements ClienteRepositorio{
-    conversor={
+    #conversor={
         toFirestore(cliente: Cliente){
             return {
                 nome: cliente.nome,
                 idade: cliente.idade,
             }
+        },
+        fromFirestore(snapshot: firebase.firestore.QueryDocumentSnapshot, options: firebase.firestore.SnapshotOptions){
+            const dados = snapshot?.data(options)
+            return new Cliente(dados.nome, dados.idade, snapshot?.id)
         }
     }
 
     async salvar(cliente: Cliente): Promise<Cliente>{
-        return null
+        if(cliente?.id){
+            await this.colecao().doc(cliente.id).set(cliente)
+            return cliente
+        }else{
+            const docRef = await this.colecao().add(cliente)
+            const doc = await docRef.get()
+            return doc.data()
+        }
     }
     async excluir(cliente: Cliente): Promise<void>{
-        return null
+        return this.colecao().doc(cliente.id).delete()
     }
     async obterTodos(): Promise<Cliente[]>{
-        return null
+        const query = await this.colecao().get()
+        return query.docs.map(doc => doc.data()) ?? []
+    }
+    private colecao(){
+        return firebase
+            .firestore().collection('clientes')
+            .withConverter(this.#conversor)
     }
 }
