e887e1e341b8f766e6ac561e7042d495980c6e10
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-10-14 14:23:13 -0300

    questao 30

--- Exercicios/questao30.js
+++ Exercicios/questao30.js
@@ -1,4 +1,5 @@
-/*Escreva um algoritmo que percorre um vetor de inteiros e defina o maior e menor valor dentro do vetor.
+/*Escreva um algoritmo que percorre um vetor de inteiros 
+e defina o maior e menor valor dentro do vetor.
  */
 
 let nums = [10, 3, 230, 4, 5, 80, 89, 100, 76]
