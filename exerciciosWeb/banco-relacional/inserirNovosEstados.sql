11a512c13ee19b7013c89d030e3c861fe8cea9fe
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-10-19 16:34:13 -0300

    Banco de Dados Relacional
    Inserir Estados com ID (Mesmo sendo auto incremento  e posivel inserir o ID manualmente)

---
+++ exerciciosWeb/banco-relacional/inserirNovosEstados.sql
@@ -0,0 +1,6 @@
+insert into estados (id, nome, sigla, regiao, populacao)
+value (1000, 'Tapajos', 'TA', 'Norte', 0.50)
+select * from estados
+
+insert into estados (nome, sigla, regiao, populacao)
+value('Carajas', 'CA', 'Norte', 0.73)
