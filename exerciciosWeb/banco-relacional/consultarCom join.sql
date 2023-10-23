b0759c87f95e38f39c99971397fc6958f5234ea9
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-10-23 13:42:50 -0300

    Banco de Dados Relacionais
    Consultar Multiplas Tabelas.

--- exerciciosWeb/banco-relacional/consultarCom join.sql
+++ exerciciosWeb/banco-relacional/consultarCom join.sql
@@ -3,3 +3,10 @@ c.nome as Cidade,
 e.regiao as regiao
  from estados e, cidade c
 where e.id = c.estado_id;
+
+select 
+    c.nome as Cidade,
+    e.nome as Estado,
+    regiao as regiao
+from estados e
+inner join cidade c on e.id = c.estado_id
