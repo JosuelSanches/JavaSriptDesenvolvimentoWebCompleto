497623f5a0fed14bddcb16446039725e5e40b1ab
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-10-24 17:03:03 -0300

    Banco de Dados Relacionais
    Inserindo empresas e seus relacionamentos com as cidades.

--- exerciciosWeb/banco-relacional/inserirEmpresas.sql
+++ exerciciosWeb/banco-relacional/inserirEmpresas.sql
@@ -9,3 +9,15 @@ values
     ('Cielo', 64667337000130);
 
 desc empresas
+desc `prefeitos`
+
+select * from empresas;
+select * from cidades;
+
+insert into empresas_unidades
+    (empresa_id, cidade_id, sede)
+values
+    (1 ,1 ,1),
+    (1, 2, 0),
+    (2, 5, 0),
+    (2, 2, 1);
